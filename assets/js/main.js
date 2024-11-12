e1e8e854f6bf2eb1a3ead863a9090e5f3b45b5dd
salman070104 <150415783+salman070104@users.noreply.github.com>
2024-11-12 23:19:44 +0700

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -451,5 +451,14 @@ function askNotificationPermission() {
     });
 }
 
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
 
 
